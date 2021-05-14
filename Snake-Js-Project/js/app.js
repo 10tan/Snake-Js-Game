dbbdbe8d07764a979f3cef83ddfe9108c414d715
Tanmay <kumartanmay109@gmail.com>
2021-05-14 11:13:38 +0530

    Nothing Done

--- Snake-Js-Project/js/app.js
+++ Snake-Js-Project/js/app.js
@@ -45,7 +45,6 @@ function gameEngine(){
         gameOverSound.play();
         musicSound.pause();
         inputDir =  {x: 0, y: 0}; 
-        alert("Game Over. Press any key to play again!");
         snakeArr = [{x: 13, y: 15}];
         musicSound.play();
         score = 0; 
