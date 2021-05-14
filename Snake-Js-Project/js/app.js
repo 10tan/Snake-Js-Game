2cc33fd6742a88324cc04ef8779458b761743e36
Tanmay <kumartanmay109@gmail.com>
2021-05-14 11:10:26 +0530

    Nothing Done

--- Snake-Js-Project/js/app.js
+++ Snake-Js-Project/js/app.js
@@ -104,7 +104,6 @@ function gameEngine(){
 
 
 // Main logic starts here
-musicSound.play();
 let hiscore = localStorage.getItem("hiscore");
 if(hiscore === null){
     hiscoreval = 0;
@@ -114,35 +113,40 @@ else{
     hiscoreval = JSON.parse(hiscore);
     hiscoreBox.innerHTML = "HiScore: " + hiscore;
 }
+musicSound.play()
 
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e =>{
     inputDir = {x: 0, y: 1} // Start the game
-    musicSound.pause()
+    
     moveSound.play();
     switch (e.key) {
         case "ArrowUp":
             console.log("ArrowUp");
             inputDir.x = 0;
             inputDir.y = -1;
+            musicSound.pause()
             break;
 
         case "ArrowDown":
             console.log("ArrowDown");
             inputDir.x = 0;
             inputDir.y = 1;
+            musicSound.pause()
             break;
 
         case "ArrowLeft":
             console.log("ArrowLeft");
             inputDir.x = -1;
             inputDir.y = 0;
+            musicSound.pause()
             break;
 
         case "ArrowRight":
             console.log("ArrowRight");
             inputDir.x = 1;
             inputDir.y = 0;
+            musicSound.pause()
             break;
         default:
             break;
